bef1b9554e0d48d70fcccce2bf603e51bab5faeb
Josh Elser <josh.elser@gmail.com>
2013-04-11 22:43:26 -0400

    Essentially writing unit tests here for guava's Iterables.partition and Iterables.limit

--- src/test/java/sorts/results/BasicIndexingTest.java
+++ src/test/java/sorts/results/BasicIndexingTest.java
@@ -2,12 +2,9 @@ package sorts.results;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.accumulo.core.client.Scanner;
-import org.apache.accumulo.core.data.Key;
-import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,10 +16,12 @@ import sorts.Sorting;
 import sorts.impl.SortingImpl;
 import sorts.options.Defaults;
 import sorts.options.Index;
+import sorts.options.Paging;
 import sorts.results.impl.MultimapQueryResult;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 
@@ -200,6 +199,62 @@ public class BasicIndexingTest extends AbstractSortableTest {
     
     scanner = c.createScanner(Defaults.METADATA_TABLE, new Authorizations("test"));
     Assert.assertEquals(1, Iterables.size(scanner));
+  }
+  
+  @Test
+  public void pagedResults() throws Exception {
+    Multimap<Column,SValue> data = HashMultimap.create();
+    
+    data.put(Column.create("TEXT"), SValue.create("foo", VIZ));
+    data.put(Column.create("TEXT"), SValue.create("bar", VIZ));
+    data.put(Column.create("TEXT"), SValue.create("foobar", VIZ));
+    
+    List<QueryResult<?>> mqrs = Lists.newArrayListWithCapacity(20);
+    for (int i = 0; i < 16; i++) {
+      mqrs.add(new MultimapQueryResult(data, Integer.toString(i), VIZ));
+    }
+    
+    SortableResult id = SortableResult.create(c, AUTHS, Collections.singleton(Index.define("TEXT")));
+    
+    Sorting s = new SortingImpl();
+    
+    s.register(id);
+    
+    s.addResults(id, mqrs);
+    
+    s.finalize(id);
+    
+    PagedQueryResult pqr = s.fetch(id, Paging.create(5, 20l));
+    
+    int pageCount = 0;
+    int numRecords = 0;
+    while (pqr.hasNext()) {
+      pageCount++;
+      int nextSize = pqr.next().size();
+      
+      Assert.assertTrue(nextSize <= 5);
+      
+      numRecords += nextSize;
+    }
+    
+    Assert.assertEquals(16, numRecords);
+    Assert.assertEquals(4, pageCount);
+    
+    pqr = s.fetch(id, Paging.create(3, 10l));
+    
+    pageCount = 0;
+    numRecords = 0;
+    while (pqr.hasNext()) {
+      pageCount++;
+      int nextSize = pqr.next().size();
+      
+      Assert.assertTrue(nextSize <= 3);
+      
+      numRecords += nextSize;
+    }
+    
+    Assert.assertEquals(10, numRecords);
+    Assert.assertEquals(4, pageCount);
   }
   
 }
