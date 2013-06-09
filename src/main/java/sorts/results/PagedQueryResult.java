222afc0959d7d7f291991bc0fe0d8e0f3cfd6784
Josh Elser <josh.elser@gmail.com>
2013-06-09 00:57:06 -0400

    Why did I have *one* class that was an Iterator and not Iterable??

--- src/main/java/sorts/results/PagedQueryResult.java
+++ src/main/java/sorts/results/PagedQueryResult.java
@@ -7,30 +7,21 @@ import sorts.options.Paging;
 
 import com.google.common.collect.Iterables;
 
-public class PagedQueryResult<T> implements Iterator<List<T>>{
+public class PagedQueryResult<T> implements Iterable<List<T>>{
 
-  protected final Iterator<List<T>> pagedLimitedResults;
+  protected final Iterable<List<T>> pagedLimitedResults;
   
   public static <T> PagedQueryResult<T> create(Iterable<T> results, Paging limits) {
     return new PagedQueryResult<T>(results, limits);
   }
   
   public PagedQueryResult(Iterable<T> results, Paging limits) {
-    pagedLimitedResults = Iterables.partition(Iterables.limit(results, limits.maxResults().intValue()), limits.pageSize()).iterator();
+    pagedLimitedResults = Iterables.partition(Iterables.limit(results, limits.maxResults().intValue()), limits.pageSize());
   }
-
+  
   @Override
-  public boolean hasNext() {
-    return pagedLimitedResults.hasNext();
+  public Iterator<List<T>> iterator() {
+    return pagedLimitedResults.iterator();
   }
-
-  @Override
-  public List<T> next() {
-    return pagedLimitedResults.next();
-  }
-
-  @Override
-  public void remove() {
-    pagedLimitedResults.remove();
-  }
+  
 }
