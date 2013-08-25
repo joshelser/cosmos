435208c747d065cbc6a21cf393dbb2255f841a95
Josh Elser <josh.elser@gmail.com>
2013-08-25 16:17:30 -0400

    Slightly cleaner javascript for most recent tracings. Some css to boot too

--- tracing-web/src/main/webapp/cosmos.js
+++ tracing-web/src/main/webapp/cosmos.js
@@ -1,32 +1,39 @@
 MOST_RECENT_URL = "rest/cosmos/recent"
+MOST_RECENT_QUERY_STRING = "?num="
+
+CONTENT_CLICKABLES = {'#most_recent_5':5, '#most_recent_10':10, '#most_recent_15':15};
+
+render_most_recent = function(js, render_to) {
+    html = ''
+
+    for (var i = 0; i < js.length; i++) {
+        html += '<div>'
+        html += '<p>' + js[i].uuid + '</p>'
+        html += '<p>' + js[i].begin + '</p>'
+        html += '<ol>'
+        for (var j = 0; j < js[i].regionTimings.length; j++) {
+            html += '<li>' + js[i].regionTimings[j].description
+                    + ' = ' + js[i].regionTimings[j].duration
+                    + 'ms</li>'
+        }
+        html += '</ol>'
+    }
+
+    render_to.html(html)
+}
+
 
 $(document).ready(function() {
-    
-    $('#most_recent').click(function() {
-        resp = $.get(MOST_RECENT_URL, {}, function(data, textStatus, jqXHR) {
-            console.log(data)
-
-            //results = jQuery.parseJSON(data)
-            results = data
-            console.log("derp")
-            console.log(results)
-            html = ''
-
-            for (var i = 0; i < results.length; i++) {
-                html += '<div>'
-                html += '<p>' + results[i].uuid + '</p>'
-                html += '<p>' + results[i].begin + '</p>'
-                html += '<ol>'
-                for (var j = 0; j < results[i].regionTimings.length; j++) {
-                    html += '<li>' + results[i].regionTimings[j].description
-                            + ' = ' + results[i].regionTimings[j].duration
-                            + 'ms</li>'
+    content = $('#content')
+    for (index in CONTENT_CLICKABLES) {
+        $(index).click(function() {
+            id = '#'+$(this)[0].id
+            resp = $.get(MOST_RECENT_URL + MOST_RECENT_QUERY_STRING + CONTENT_CLICKABLES[id], {}, 
+                function(data, textStatus, jqXHR) {
+                    render_most_recent(data, content)
                 }
-                html += '</ol>'
-            }
-
-            $('#content').html(html)
+            );
         });
-    });
+    }
 
 });
